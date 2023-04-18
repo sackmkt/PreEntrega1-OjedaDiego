54d9bbe7ec99b2caae6a86089d408e80f99c01f6
sackmkt <diego95ojeda@gmail.com>
2023-04-17 23:47:58 -0300

    If y funcion

---
+++ main.js
@@ -0,0 +1,21 @@
+function InicioPagina(){ 
+alert("Bienvenido al primer trabajo del curso");
+
+alert("Dinos, cuales son tus espectativas por aprender este lenguaje?");
+
+let respuesta = prompt("Del 1 al 5, que numero pondrias: ");
+
+if (respuesta <=5){
+    alert("El numero esta perfecto, vamos a evaluarte");
+} else {
+    alert("NO ingresaste el numero dentro de lo solicitado, intentalo de nuevo");
+    InicioPagina
+}
+ 
+}
+
+// switch(respuesta){
+
+// }
+
+
